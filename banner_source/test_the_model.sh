c3c5f089bd6d8c691557113bcd2c3601c2c2576e
Wojciech Migda <wojtek.golf@interia.pl>
2015-03-11 18:49:48 +0100

    helper scripts

---
+++ banner_source/test_the_model.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+java -cp 'lib/*' BANNER_BioC config/banner_bioc.xml data/test_file.xml out.xml
---
+++ banner_source/train_a_model.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+java -cp 'lib/*' banner.eval.BANNER train config/banner_bioc.xml
